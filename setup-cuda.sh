d53edf540e0684369387a38eab504dfe0e75ba83
mrq <barry.quiggles@protonmail.com>
2023-02-16 19:48:06 +0000

    pip-ifying things

--- setup-cuda.sh
+++ setup-cuda.sh
@@ -4,4 +4,5 @@ python -m pip install --upgrade pip
 # CUDA
 pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
 python -m pip install -r ./requirements.txt
+python -m pip install -r ./requirements_legacy.txt
 deactivate
